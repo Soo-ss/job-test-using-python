7d6e91b02e680af52d28b3a9e7750f8351c3daf7
Soo-ss <kms334488@naver.com>
2020-09-02 00:57:10 +0900

    windows

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-import requests
-
-res = requests.get(url="https://jsonplaceholder.typicode.com/users")
-
-data = res.json()
-
-name_list = []
-for user in data:
-    name_list.append(user["name"])
-
-print(name_list)
+print("window test")
