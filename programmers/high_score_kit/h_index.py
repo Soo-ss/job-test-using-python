48c9f123efa176c09fa4328617962534a2da0c33
Soo-ss <kms334488@naver.com>
2021-03-18 01:01:08 +0900

    21-03-18

--- programmers/high_score_kit/h_index.py
+++ programmers/high_score_kit/h_index.py
@@ -1,10 +1,12 @@
 def solution(citations):
-    answer = 0
-
     citations.sort()
-    answer = citations[len(citations) // 2]
-    print(answer)
-    return answer
+    length = len(citations)
+
+    for i in range(length):
+        if citations[i] >= length - i:
+            return length - i
+
+    return 0
 
 
-solution([1, 1, 3, 5, 5, 5])
+print(solution([1, 1, 3, 5, 5, 5]))
