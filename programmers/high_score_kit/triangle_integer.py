9bedc6c500eae193a75ee04fcba42bb11e46fbf2
Soo-ss <kms334488@naver.com>
2021-06-10 22:24:49 +0900

    mother

---
+++ programmers/high_score_kit/triangle_integer.py
@@ -0,0 +1,10 @@
+def solution(triangle):
+    answer = 0
+
+    for item in triangle:
+        print(item)
+
+    return answer
+
+
+solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]])
