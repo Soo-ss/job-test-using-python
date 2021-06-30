d991cb6049e774fe1839fcc40d67b13e626e4f71
Soo-ss <kms334488@naver.com>
2021-06-30 10:38:17 +0900

    if do not using regex

--- programmers/etc/new_id.py
+++ programmers/etc/new_id.py
@@ -1,6 +1,37 @@
+def one(new_id):
+    return new_id.lower()
+
+
+def two(new_id):
+    return new_id
+
+
+def three(new_id):
+    return new_id
+
+
+def four(new_id):
+    return new_id
+
+
+def five(new_id):
+    return new_id
+
+
+def six(new_id):
+    return new_id
+
+
+def seven(new_id):
+    return new_id
+
+
 def solution(new_id):
     answer = ""
+
+    answer = one(new_id)
+
     return answer
 
 
-solution("...!@BaT#*..y.abcdefghijklm")
+print(solution("...!@BaT#*..y.abcdefghijklm"))
