4410649b882cb699b5ca7e65dbade0d03517f498
Soo-ss <kms334488@naver.com>
2021-07-11 23:00:06 +0900

    21.07.11

--- programmers/etc/hate_same_num.py
+++ programmers/etc/hate_same_num.py
@@ -1,9 +1,27 @@
+def no_continuous1(s):
+    a = []
+    for i in s:
+        if a[-1:] == [i]:
+            continue
+        a.append(i)
+    return a
+
+
+def no_continuous2(s):
+    result = []
+    for c in s:
+        if (len(result) == 0) or (result[-1] != c):
+            result.append(c)
+    return result
+
+
 def solution(arr):
     answer = []
+    answer.append(arr[0])
 
-    for i in range(len(arr) - 1):
-        if arr[i] == arr[i + 1]:
-            del arr[i]
+    for i in range(1, len(arr)):
+        if arr[i - 1] != arr[i]:
+            answer.append(arr[i])
 
     return answer
 
